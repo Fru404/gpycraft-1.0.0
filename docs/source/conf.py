e0d18c7c4edbf0df64ef126531499e68084d6b67
Fru Ngwa <108280487+Fru404@users.noreply.github.com>
2024-01-15 09:32:40 +0000

    uncomment_better

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-#import better
+import better
 import os
 import sys
 
